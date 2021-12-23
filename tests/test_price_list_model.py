0187375a687ded7a26c17d4a2b3e4356770d7d92
Bartosz Kisly <01169184@pw.edu.pl>
2021-12-23 01:51:31 +0100

    Created separate tests file for PriceListModel and made a blueprint for it

---
+++ tests/test_price_list_model.py
@@ -0,0 +1,13 @@
+# Tests for PriceListModel.__init__()
+
+# @TODO: Write tests that check _read_pricing() and _pricing_validation()
+# functionality
+
+# Tests for PriceListModel.get_pricing()
+
+def test_price_list_model_get_typical():
+    pass
+
+
+def test_price_list_model_get_wrong_service():
+    pass
