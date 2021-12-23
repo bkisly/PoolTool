ea2c52eb9c1cfc43f6733a460afb1cb43d1e60b3
Bartosz Kisly <01169184@pw.edu.pl>
2021-12-23 01:24:26 +0100

    Added JSON parsers for value_types classes

--- model/value_types.py
+++ model/value_types.py
@@ -84,6 +84,12 @@ class Price:
         else:
             return False
 
+    @staticmethod
+    def from_json(json_dict: dict):
+        zl = json_dict["zl"]
+        gr = json_dict["gr"]
+        return Price(zl, gr)
+
     def _get_total_gr(self, price=None):
         if price is None:
             price = self
@@ -131,6 +137,16 @@ class HoursRange:
         result_begin = min(self.begin, other.begin)
         result_end = max(self.end, other.end)
         return HoursRange(result_begin, result_end)
+
+    @staticmethod
+    def from_json(json_dict: dict):
+        begin_hour = json_dict["begin"]["hour"]
+        begin_minute = json_dict["begin"]["minute"]
+        end_hour = json_dict["end"]["hour"]
+        end_minute = json_dict["end"]["minute"]
+
+        return HoursRange(
+            time(begin_hour, begin_minute), time(end_hour, end_minute))
 
     def _data_validation(self, begin, end):
         if not (isinstance(begin, time) and isinstance(end, time)):
