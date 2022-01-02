f798b516507aa48fd8899d7e441617c0c47c4dda
Bartosz Kisly <01169184@pw.edu.pl>
2022-01-02 12:16:44 +0100

    Created tests file for PoolModel and wrote blueprints for it

---
+++ tests/test_pool_model.py
@@ -0,0 +1,22 @@
+# Tests for PoolModel.__init__()
+
+def test_pool_model_init_typical():
+    pass
+
+
+def test_pool_model_init_invalid_working_hours():
+    pass
+
+
+def test_pool_model_init_empty_working_hours():
+    pass
+
+
+def test_pool_model_wrong_day():
+    pass
+
+
+# Tests for PoolModel.next_day()
+
+def test_pool_model_next_day():
+    pass
