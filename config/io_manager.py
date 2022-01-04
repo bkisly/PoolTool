898144951a38067aac32a1329d9b5ce891f48bd1
Bartosz Kisly <01169184@pw.edu.pl>
2022-01-05 00:16:08 +0100

    Implemented initial version of PoolModel serializers

--- config/io_manager.py
+++ config/io_manager.py
@@ -1,14 +1,16 @@
+from datetime import date
 from model.pool_model import PoolModel
-from model.price_list_model import PriceListModel
 import json
 
 
 def write_pool_model(handle, pool_model) -> None:
-    pass
+    json_dict = PoolModel.to_json(pool_model)
+    json.dump(json_dict, handle)
 
 
-def read_pool_model(handle) -> PoolModel:
-    pass
+def read_pool_model(handle, current_day: date) -> PoolModel:
+    json_dict = json.load(handle)
+    return PoolModel(json_dict, current_day)
 
 
 def write_config(handle, admin) -> None:
