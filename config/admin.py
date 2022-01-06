e49593edd70bbb1c7268dacace8b645426cdf7e7
Bartosz Kisly <01169184@pw.edu.pl>
2022-01-07 00:36:12 +0100

    Finished implementing Admin class (first version). Tests passed

--- config/admin.py
+++ config/admin.py
@@ -1,5 +1,15 @@
+from datetime import date, timedelta
+
+
 class Admin:
-    def __init__(self) -> None:
-        # Get the current day from the config file
-        # and assign it as an attribute
-        pass
+    def __init__(self, current_day: date) -> None:
+        if not isinstance(current_day, date):
+            raise ValueError("Current day must be a date class instance")
+
+        self.current_day = current_day
+
+    def set_current_pool(self, path: str):
+        self.current_pool_path = path
+
+    def next_day(self):
+        self.current_day += timedelta(days=1)
--- tests/test_admin.py
+++ tests/test_admin.py
@@ -1,4 +1,15 @@
+from config.admin import Admin
+from datetime import date
+import pytest
+
+
 # Tests for Admin.__init__()
 
 def test_admin_init_typical():
-    pass
+    admin = Admin(date(2022, 1, 1))
+    assert admin.current_day == date(2022, 1, 1)
+
+
+def test_admin_init_wrong_day_type():
+    with pytest.raises(ValueError):
+        Admin("abcds")
