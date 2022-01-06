fbaa5c7192572806f08fea354a72b2dbd11c2c3c
Bartosz Kisly <01169184@pw.edu.pl>
2022-01-07 00:26:07 +0100

    Wrote test for read_pool_model(), passed

--- tests/test_io_manager.py
+++ tests/test_io_manager.py
@@ -1,4 +1,4 @@
-from config.io_manager import write_pool_model
+from config.io_manager import read_pool_model, write_pool_model
 from io import StringIO
 from model.pool_model import PoolModel
 from datetime import date
@@ -117,3 +117,115 @@ def test_io_write_pool_model_correct():
     saved_dict = ast.literal_eval(handle.getvalue())
 
     assert saved_dict == pool_json
+
+
+# Tests for io_manager.read_pool_model()
+
+def test_io_read_pool_model_correct():
+    pool_json = {
+        "name": "MyPool",
+        "lanes_amount": 5,
+        "working_hours": {
+            "0": {
+                "begin": {
+                    "hour": 9,
+                    "minute": 0
+                },
+                "end": {
+                    "hour": 18,
+                    "minute": 0
+                }
+            },
+            "1": {
+                "begin": {
+                    "hour": 10,
+                    "minute": 0
+                },
+                "end": {
+                    "hour": 18,
+                    "minute": 30
+                }
+            },
+        },
+        "price_list": [
+            {
+                "service": 0,
+                "day": 0,
+                "hours_range": {
+                    "begin": {
+                        "hour": 9,
+                        "minute": 0
+                    },
+                    "end": {
+                        "hour": 18,
+                        "minute": 0
+                    }
+                },
+                "price": {
+                    "zl": 2,
+                    "gr": 50
+                }
+            },
+            {
+                "service": 1,
+                "day": 0,
+                "hours_range": {
+                    "begin": {
+                        "hour": 9,
+                        "minute": 0
+                    },
+                    "end": {
+                        "hour": 18,
+                        "minute": 0
+                    }
+                },
+                "price": {
+                    "zl": 2,
+                    "gr": 50
+                }
+            },
+            {
+                "service": 0,
+                "day": 1,
+                "hours_range": {
+                    "begin": {
+                        "hour": 10,
+                        "minute": 0
+                    },
+                    "end": {
+                        "hour": 18,
+                        "minute": 30
+                    }
+                },
+                "price": {
+                    "zl": 2,
+                    "gr": 50
+                }
+            },
+            {
+                "service": 1,
+                "day": 1,
+                "hours_range": {
+                    "begin": {
+                        "hour": 10,
+                        "minute": 0
+                    },
+                    "end": {
+                        "hour": 18,
+                        "minute": 30
+                    }
+                },
+                "price": {
+                    "zl": 2,
+                    "gr": 50
+                }
+            },
+        ]
+    }
+    handle = StringIO(str(pool_json).replace("'", '"'))
+
+    pool_model = read_pool_model(handle, date(2022, 1, 1))
+    pool_json["reservations"] = []
+
+    assert PoolModel.to_json(pool_model) == pool_json
+    assert pool_model.current_day == date(2022, 1, 1)
