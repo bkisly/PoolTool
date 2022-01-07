34b3eaa3b047d3c7804108bb2e878ce0b934eae0
Bartosz Kisly <01169184@pw.edu.pl>
2022-01-07 01:24:43 +0100

    Created blueprints for next 2 functions available in the io_manager module

--- config/io_manager.py
+++ config/io_manager.py
@@ -21,3 +21,15 @@ def write_config(handle, admin) -> None:
 def read_config(handle) -> Admin:
     json_dict = json.load(handle)
     return Admin.from_json(json_dict)
+
+
+def does_config_exist(current_directory: str) -> bool:
+    # Check if there's a config.json file in the current directory.
+    # Raise an exception if the given directory is not valid
+    pass
+
+
+def get_pools_files(current_directory: str) -> list:
+    # Returns a list of .json files in the current directory with name
+    # starting with "pool_"
+    pass
