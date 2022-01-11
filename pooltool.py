697bfbf432b674869e0cdd789b7b430540684be4
Bartosz Kisły <01169184@pw.edu.pl>
2022-01-11 12:51:46 +0100

    Implemented argument parser and validation for entry point

--- pooltool.py
+++ pooltool.py
@@ -1,8 +1,22 @@
 import sys
+import argparse
 
 
-def main(args):
-    pass
+def main(args: list[str]):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p")
+    parser.add_argument("-a", action="store_true")
+    parsed_args = parser.parse_args(args[1:])
+    _validate_arguments(vars(parsed_args))
+
+    print(parsed_args)
+
+
+def _validate_arguments(args: dict) -> None:
+    if args["p"] and args["a"]:
+        raise ValueError("Cannot execute with both -a and -p given.")
+    elif not args["p"] and not args["a"]:
+        raise ValueError("Cannot launch without any arguments given")
 
 
 if __name__ == "__main__":
