e58079a19ad905c977393c8c5bfa6e3febe313f8
Bartosz Kisly <01169184@pw.edu.pl>
2022-01-12 21:45:55 +0100

    Implemented main method for the pool view

--- view/pool_view.py
+++ view/pool_view.py
@@ -3,6 +3,7 @@ from config.io_manager import (
     read_config, does_config_exist)
 from config.admin import Admin
 from model.pool_model import PoolModel
+from view.operations_view import print_operations
 
 
 def _config_initialization() -> Admin:
@@ -41,27 +42,71 @@ def _pool_initialization(pool_path: str) -> PoolModel:
     return pool_model
 
 
-def _add_reservation():
+def _add_reservation(pool_model: PoolModel):
     pass
 
 
-def _view_reservations():
+def _view_reservations(pool_model: PoolModel):
     pass
 
 
-def _view_price_list():
+def _view_price_list(pool_model: PoolModel):
     pass
 
 
-def _view_tickets_amount():
+def _view_working_hours(pool_model: PoolModel):
     pass
 
 
-def _view_free_lanes():
+def _get_financial_report(pool_model: PoolModel):
+    pass
+
+
+def _view_tickets_amount(pool_model: PoolModel):
+    pass
+
+
+def _view_free_lanes(pool_model: PoolModel):
     pass
 
 
 def pool_view(pool_path: str) -> None:
     pool_model = _pool_initialization(pool_path)
-    print(pool_model.name)
-    # @TODO: PoolModel reading done, continue with match-case for all options
+    print("POOLTOOL - POOL MANAGEMENT SYSTEM\n")
+    print("Welcome to PoolTool!")
+    print(
+        f"Active pool: {pool_model.name}. "
+        + f"Current day: {pool_model.current_day}\n")
+
+    actions = [
+        "Add reservation",
+        "View reservations",
+        "Price list",
+        "Working hours",
+        "Financial report",
+        "Available tickets",
+        "Available lanes",
+        "Exit"
+    ]
+
+    exit_selected = False
+    while not exit_selected:
+        selected_index = print_operations(actions)
+
+        match selected_index:
+            case 0:
+                _add_reservation(pool_model)
+            case 1:
+                _view_reservations(pool_model)
+            case 2:
+                _view_price_list(pool_model)
+            case 3:
+                _view_working_hours(pool_model)
+            case 4:
+                _get_financial_report(pool_model)
+            case 5:
+                _view_tickets_amount(pool_model)
+            case 6:
+                _view_free_lanes(pool_model)
+            case 7:
+                exit_selected = True
