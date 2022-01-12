f589ef350301fe300fe1ed9695d403485cd59e24
Bartosz Kisly <01169184@pw.edu.pl>
2022-01-12 14:13:16 +0100

    Clarified code for the admin view

--- view/admin_view.py
+++ view/admin_view.py
@@ -4,40 +4,51 @@ from config.io_manager import read_config, write_config, does_config_exist
 from view.operations_view import print_operations
 
 
-def admin_view():
+def _config_validation() -> Admin:
+    with open("config.json") as handle:
+        try:
+            admin = read_config(handle)
+        except Exception as e:
+            print("An error occurred while loading config file:")
+            print({e.args[0]})
+            exit()
+
+    return admin
+
+
+def _config_initialization() -> Admin:
+    print(
+        "There's no config file detected, "
+        + "preparing for the first launch...")
+    print(
+        "In order to launch the program properly, "
+        + "you need to specify current day.")
+
+    try:
+        year = int(input("Enter current year: "))
+        month = int(input("Enter current month: "))
+        day = int(input("Enter current day: "))
+
+        admin = Admin(date(int(year), int(month), int(day)))
+    except Exception as e:
+        print("An error occurred while creating config file.")
+        print(f"{e.args[0]}")
+        exit()
+
+    with open("config.json", "w") as handle:
+        write_config(handle, admin)
+
+    print()
+    return admin
+
+
+def admin_view() -> None:
     admin = None
 
     if does_config_exist(".\\"):
-        with open("config.json") as handle:
-            try:
-                admin = read_config(handle)
-            except Exception as e:
-                print("An error occurred while loading config file:")
-                print({e.args[0]})
-                return
+        admin = _config_validation()
     else:
-        print(
-            "There's no config file detected, "
-            + "preparing for the first launch...")
-        print(
-            "In order to launch the program properly, "
-            + "you need to specify current day.")
-
-        try:
-            year = int(input("Enter current year: "))
-            month = int(input("Enter current month: "))
-            day = int(input("Enter current day: "))
-
-            admin = Admin(date(int(year), int(month), int(day)))
-        except Exception as e:
-            print("An error occurred while creating config file.")
-            print(f"{e.args[0]}")
-            return
-
-        with open("config.json", "w") as handle:
-            write_config(handle, admin)
-
-        print()
+        admin = _config_initialization()
 
     print(f"PoolTool administration panel. Current day: {admin.current_day}")
 
@@ -56,5 +67,3 @@ def admin_view():
                 + f"Current day: {admin.current_day}")
         else:
             exit_selected = True
-
-# @TODO: Clarify Admin view code
