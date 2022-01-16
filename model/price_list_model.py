fd29d6e68b1d82865aa328eede7910941d52b182
Bartosz Kisly <01169184@pw.edu.pl>
2022-01-16 18:50:19 +0100

    Added documentation for price_list_model_classes

--- model/price_list_model.py
+++ model/price_list_model.py
@@ -4,6 +4,11 @@ from model.value_types import Price, HoursRange, Services, WeekDay
 
 
 class PriceListPosition:
+    """
+    Represents a single position on a price list. Stores information about
+    service type, week day, hours range and reservation price per hour.
+    """
+
     def __init__(
         self, service: Services, day: WeekDay,
         hours_range: HoursRange, price: Price
@@ -31,6 +36,11 @@ class PriceListPosition:
 
     @staticmethod
     def from_json(json_dict: dict):
+        """
+        Converts a JSON-formatted dictionary to a PriceListPosition object
+        and returns it.
+        """
+
         service = Services(json_dict["service"])
         day = WeekDay(json_dict["day"])
         hours_range = HoursRange.from_json(json_dict["hours_range"])
@@ -40,6 +50,11 @@ class PriceListPosition:
 
     @staticmethod
     def to_json(object) -> dict:
+        """
+        Converts a PriceListPosition object to a JSON-formatted dictionary
+        and returns it.
+        """
+
         json_dict = {
             "service": object.service.value,
             "day": object.day.value,
@@ -49,7 +64,12 @@ class PriceListPosition:
 
         return json_dict
 
-    def _data_validation(self, hours_range, price):
+    def _data_validation(self, hours_range: HoursRange, price: Price) -> None:
+        """
+        Validates initial data and throws proper exceptions if the given data
+        is invalid.
+        """
+
         if not isinstance(hours_range, HoursRange):
             raise TypeError("Range of hours must be HoursRange instance")
 
@@ -58,11 +78,27 @@ class PriceListPosition:
 
 
 class PriceListModel:
-    def __init__(self, working_hours: dict, pricing_json: list) -> None:
+    """
+    Represents the price list. Stores a list of PriceListPosition objects.
+    Needs to be initialized using a JSON-formatted list of PriceListPosition
+    objects.
+    """
+
+    def __init__(
+        self, working_hours: dict[WeekDay, HoursRange],
+        pricing_json: list
+    ) -> None:
+
         self._pricing = PriceListModel.from_json(pricing_json)
         self._price_list_validation(working_hours)
 
-    def get_pricing(self, service: Services = None) -> list:
+    def get_pricing(self, service: Services = None) -> list[PriceListPosition]:
+        """
+        Returns the price list (list of PriceListPosition objects). If service
+        is given, returns a list of only these objets with equal service
+        attribute (so only the price list for individuals or swimming schools)
+        """
+
         if service is None:
             return self._pricing
 
@@ -75,7 +111,12 @@ class PriceListModel:
         return filtered_positions
 
     @staticmethod
-    def from_json(pricing_json: list) -> list:
+    def from_json(pricing_json: list) -> list[PriceListPosition]:
+        """
+        Converts a list of JSON-formatted PriceListPosition objects
+        to the list of PriceListPosition objects and returns it.
+        """
+
         pricing = []
 
         if not pricing_json:
@@ -87,16 +128,25 @@ class PriceListModel:
         return pricing
 
     @staticmethod
-    def to_json(object) -> list:
+    def to_json(pricing: list[PriceListPosition]) -> list:
+        """
+        Converts a list of PriceListPosition objects to the JSON-formatted
+        list of PriceListPosition objects.
+        """
+
         json_list = []
 
-        for position in object:
+        for position in pricing:
             json_list.append(PriceListPosition.to_json(position))
 
         return json_list
 
-    def _price_list_validation(self, working_hours: dict):
-        # Working hours is a dict WeekDay -> HoursRange
+    def _price_list_validation(
+        self, working_hours: dict[WeekDay, HoursRange]
+    ) -> None:
+        """
+        Throws an exception if price list doesn't match working hours.
+        """
 
         # 1. Get the lists of PriceListPosition objects
         # separately for individuals and schools
